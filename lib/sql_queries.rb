f06deb798cb8ee19fc52854530b83a3dff9a6eec
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-11 19:43:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name;"
 end
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY acs; "
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ACS; "
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -16,7 +16,6 @@ end
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
 end
-
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
 end
